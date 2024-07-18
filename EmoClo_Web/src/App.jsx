625ae26b03fcaa2ad83a2347703e458fab3bc4f8
gautamKumar <gautamkannoujiya58@gmail.com>
2024-07-18 16:47:44 +0530

    Project initiated

---
+++ EmoClo_Web/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-gray-800 text-white m-2 p-2">EmoClo</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
